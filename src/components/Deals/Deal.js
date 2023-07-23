b414dbae4fa49ec51f7b8a65246604dc2218a3a4
Natasha <nmalyushenko@mail.ru>
2023-07-23 16:55:55 +1000

    deal list became a table with rows and cells

--- src/components/Deals/Deal.js
+++ src/components/Deals/Deal.js
@@ -1,14 +1,29 @@
 import styles from './Deal.module.css'
+import Stack from 'react-bootstrap/Stack'
 
 function Deal({ name, ticker, profit, profitInPercent }) {
   return (
-    <div className={styles.deal}>
-      <h5>{name}</h5>
-      <small>{ticker}</small>
-      <small>
-        {profit} ({profitInPercent}%){' '}
-      </small>
+    // <div className={styles.deal}>
+    //   <div className={styles.dealName}>{name}</div>
+    //   <div className={styles.dealTicker}>{ticker} </div>
+    //   <div className={styles.dealProfit}>{profit}</div>
+    //   <div className={styles.dealProfit}>{profitInPercent}%</div>
+
+    <div className={styles.table}>
+      <div className={styles.row}>
+        <div className={styles.cell}>
+          <div className={styles.dealName}>{name}</div>
+          <div className={styles.dealTicker}>{ticker}</div>
+        </div>
+        {
+          <div className={styles.cell}>
+            <div className={styles.dealProfit}>{profit}</div>
+            <div className={styles.dealProfitInPercent}>{profitInPercent}%</div>
+          </div>
+        }
+      </div>
     </div>
+    //</div>
   )
 }
 
