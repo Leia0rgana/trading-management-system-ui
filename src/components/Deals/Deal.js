3fcba145ee52023ed3f5aef2c62e07efb26bbfc0
Natasha <nmalyushenko@mail.ru>
2023-07-23 19:08:10 +1000

    table classname were deleted

--- src/components/Deals/Deal.js
+++ src/components/Deals/Deal.js
@@ -1,29 +1,23 @@
 import styles from './Deal.module.css'
-import Stack from 'react-bootstrap/Stack'
 
 function Deal({ name, ticker, profit, profitInPercent }) {
   return (
-    // <div className={styles.deal}>
-    //   <div className={styles.dealName}>{name}</div>
-    //   <div className={styles.dealTicker}>{ticker} </div>
-    //   <div className={styles.dealProfit}>{profit}</div>
-    //   <div className={styles.dealProfit}>{profitInPercent}%</div>
-
-    <div className={styles.table}>
-      <div className={styles.row}>
-        <div className={styles.cell}>
-          <div className={styles.dealName}>{name}</div>
-          <div className={styles.dealTicker}>{ticker}</div>
+    <div className={styles.row}>
+      <div className={styles.cell}>
+        <div className={styles.dealName}>{name}</div>
+        <div className={styles.dealTicker}>{ticker}</div>
+      </div>
+      <div className={styles.cell}>
+        <div
+          className={`${styles.dealProfit} ${
+            profit < 0 ? styles.negativeProfit : styles.positiveProfit
+          }`}
+        >
+          {profit}
         </div>
-        {
-          <div className={styles.cell}>
-            <div className={styles.dealProfit}>{profit}</div>
-            <div className={styles.dealProfitInPercent}>{profitInPercent}%</div>
-          </div>
-        }
+        <div className={styles.dealProfitInPercent}>{profitInPercent}%</div>
       </div>
     </div>
-    //</div>
   )
 }
 
