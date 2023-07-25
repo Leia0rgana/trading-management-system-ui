16c3584c3c22aebc66a99d5b6b0fc0ff930c5ffe
Natasha <nmalyushenko@mail.ru>
2023-07-25 21:25:04 +1000

    bootstrap accordion component was applied

--- src/components/Deals/Deal.js
+++ src/components/Deals/Deal.js
@@ -1,23 +1,33 @@
 import styles from './Deal.module.css'
+import { Accordion } from 'react-bootstrap'
 
 function Deal({ name, ticker, profit, profitInPercent }) {
   return (
-    <div className={styles.row}>
-      <div className={styles.cell}>
-        <div className={styles.dealName}>{name}</div>
-        <div className={styles.dealTicker}>{ticker}</div>
-      </div>
-      <div className={styles.cell}>
-        <div
-          className={`${styles.dealProfit} ${
-            profit < 0 ? styles.negativeProfit : styles.positiveProfit
-          }`}
-        >
-          {profit}
-        </div>
-        <div className={styles.dealProfitInPercent}>{profitInPercent}%</div>
-      </div>
-    </div>
+    <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>
+          <div className={styles.row}>
+            <div className={styles.cell}>
+              <div className={styles.dealName}>{name}</div>
+              <div className={styles.dealTicker}>{ticker}</div>
+            </div>
+            <div className={styles.cell}>
+              <div
+                className={`${styles.dealProfit} ${
+                  profit < 0 ? styles.negativeProfit : styles.positiveProfit
+                }`}
+              >
+                {profit}
+              </div>
+              <div className={styles.dealProfitInPercent}>
+                {profitInPercent}%
+              </div>
+            </div>
+          </div>
+        </Accordion.Header>
+        <Accordion.Body></Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   )
 }
 
