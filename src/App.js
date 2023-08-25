4db4f4cf9c689f0e3750735bc8fded1521b53102
Natasha <nmalyushenko@mail.ru>
2023-08-25 16:07:33 +0300

    button click functionality was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import DealList from './components/Deals/DealList'
+import DealNamesList from './components/Deals/DealNamesList'
 
 function App() {
   return (
     <div className="App">
       <h1>Фьючерсы</h1>
+      <DealNamesList />
       <DealList />
     </div>
   )
--- src/components/Deals/DealNamesList.js
+++ src/components/Deals/DealNamesList.js
@@ -2,9 +2,12 @@ import { useState, useEffect } from 'react'
 import DealName from './DealName'
 import axios from 'axios'
 import styles from './DealNamesList.module.css'
+import Button from 'react-bootstrap/Button'
 
 function DealNamesList() {
   const [names, setNames] = useState([])
+  const [selectedNames, setSelectedNames] = useState([])
+  const [selectedButtons, setSelectedButtons] = useState([])
 
   useEffect(() => {
     async function fetchData() {
@@ -15,10 +18,39 @@ function DealNamesList() {
     fetchData()
   }, [])
 
+  const selectedNamesHandler = (selectedName) => {
+    if (selectedNames.includes(selectedName)) {
+      const updatedSelectedNames = selectedNames.filter(
+        (newItem) => newItem !== selectedName
+      )
+      setSelectedNames(updatedSelectedNames)
+    } else {
+      setSelectedNames([...selectedNames, selectedName])
+    }
+  }
+
+  const getSelectedButtonsIndexes = (index) => {
+    selectedButtons.includes(index)
+      ? setSelectedButtons(
+          selectedButtons.filter((btnIndex) => btnIndex !== index)
+        )
+      : setSelectedButtons([...selectedButtons, index])
+  }
+
   return (
     <div className={styles.namesList}>
       {names.map((name, index) => (
-        <DealName key={index} name={name.futureName} />
+        <Button
+          key={index}
+          className={styles.button}
+          variant={selectedButtons.includes(index) ? 'success' : 'light'}
+          onClick={() => {
+            getSelectedButtonsIndexes(index)
+            selectedNamesHandler(name.futureName)
+          }}
+        >
+          <DealName key={index} name={name.futureName} />
+        </Button>
       ))}
     </div>
   )
