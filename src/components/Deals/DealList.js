148622dbec738533d1f35cf97e2ee5da2f1dce5d
Natasha <nmalyushenko@mail.ru>
2023-08-27 17:25:28 +0300

    grouping deals by date was added

--- src/components/Deals/DealList.js
+++ src/components/Deals/DealList.js
@@ -2,6 +2,13 @@ import { useState, useEffect } from 'react'
 import Deal from './Deal'
 import axios from 'axios'
 
+class DealsByDate {
+  constructor(date, deals) {
+    this.date = date
+    this.deals = deals
+  }
+}
+
 function DealList() {
   const [deals, setDeals] = useState([])
 
@@ -14,23 +21,84 @@ function DealList() {
     fetchData()
   }, [])
 
+  const getUniqueDates = (deals) => {
+    const uniqueDates = []
+
+    for (let i = 0; i < deals.length; i++) {
+      const date = deals[i].dateTime.split('T')[0]
+
+      !uniqueDates.includes(date) && uniqueDates.push(date)
+    }
+
+    return uniqueDates
+  }
+
+  const matchDealsWithDate = (deals, date) => {
+    return deals.filter((deal) => {
+      return deal.dateTime.split('T')[0] === date
+    })
+  }
+
+  const getDealsByDates = (dates) => {
+    const dealsByDates = []
+
+    for (let i = 0; i < dates.length; i++) {
+      dealsByDates.push(
+        new DealsByDate(dates[i], matchDealsWithDate(deals, dates[i]))
+      )
+    }
+    return dealsByDates
+  }
+
+  let currentDate = null
+
   return (
     <>
-      {deals.map((deal) => (
-        <Deal
-          key={deal.id}
-          name={deal.name}
-          ticker={deal.ticker}
-          profit={deal.profit}
-          profitInPercent={deal.profitInPercents}
-          sum={deal.sum}
-          price={deal.price}
-          orderDirection={deal.orderDirection}
-          dateTime={deal.dateTime}
-          quantity={deal.quantity}
-          closedDeal={deal.closedDeal}
-        />
-      ))}
+      {getDealsByDates(getUniqueDates(deals)).map((item) => {
+        if (item.date !== currentDate) {
+          currentDate = item.date
+          return (
+            <div key={item.date}>
+              <h3>{item.date}</h3>
+              {item.deals.map((deal) => (
+                <Deal
+                  key={deal.id}
+                  name={deal.name}
+                  ticker={deal.ticker}
+                  profit={deal.profit}
+                  profitInPercent={deal.profitInPercents}
+                  sum={deal.sum}
+                  price={deal.price}
+                  orderDirection={deal.orderDirection}
+                  dateTime={deal.dateTime}
+                  quantity={deal.quantity}
+                  closedDeal={deal.closedDeal}
+                />
+              ))}
+            </div>
+          )
+        } else {
+          return (
+            <div key={item.date}>
+              {item.deals.map((deal) => (
+                <Deal
+                  key={deal.id}
+                  name={deal.name}
+                  ticker={deal.ticker}
+                  profit={deal.profit}
+                  profitInPercent={deal.profitInPercents}
+                  sum={deal.sum}
+                  price={deal.price}
+                  orderDirection={deal.orderDirection}
+                  dateTime={deal.dateTime}
+                  quantity={deal.quantity}
+                  closedDeal={deal.closedDeal}
+                />
+              ))}
+            </div>
+          )
+        }
+      })}
     </>
   )
 }
