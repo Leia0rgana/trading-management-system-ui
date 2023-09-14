07d74439b3da3031ffb2074b1bd608d6f831fdfe
Natasha <nmalyushenko@mail.ru>
2023-09-14 21:52:17 +0300

    formatting was fixed

--- src/components/Deals/OpenedDealList.js
+++ src/components/Deals/OpenedDealList.js
@@ -4,16 +4,18 @@ export default function OpenedDealList({ openedDeals }) {
   return (
     <>
       {openedDeals.map((openedDeal) => (
-        <OpenedDeal
-          key={openedDeal.id}
-          name={openedDeal.name}
-          ticker={openedDeal.ticker}
-          sum={openedDeal.sum}
-          price={openedDeal.price}
-          orderDirection={openedDeal.orderDirection}
-          dateTime={openedDeal.dateTime}
-          quantity={openedDeal.quantity}
-        />
+        <div style={{ marginBlockEnd: '10px' }}>
+          <OpenedDeal
+            key={openedDeal.id}
+            name={openedDeal.name}
+            ticker={openedDeal.ticker}
+            sum={openedDeal.sum}
+            price={openedDeal.price}
+            orderDirection={openedDeal.orderDirection}
+            dateTime={openedDeal.dateTime}
+            quantity={openedDeal.quantity}
+          />
+        </div>
       ))}
     </>
   )
