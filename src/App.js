eeba36f180dd153527f53bb351b75986505482a5
Natasha <nmalyushenko@mail.ru>
2023-10-02 22:48:12 +0300

    sidebar funcionality was added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import OpenedDealList from './components/Deals/OpenedDealList'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { baseURL } from './components/Deals/ClosedDealList'
+import Sidebar from './components/UI/Sidebar'
 
 export default function App() {
   const [clickedDealNames, setClickedDealNames] = useState([])
   const [buttonStyle, setButtonStyle] = useState('light')
   const [openedDeals, setOpenedDeals] = useState([])
+  const [sidebarExpanded, setSidebarExpanded] = useState(false)
 
   useEffect(() => {
     async function fetchData() {
@@ -34,28 +36,33 @@ export default function App() {
     setButtonStyle(buttonStyle === 'light' ? 'success' : 'light')
   }
 
+  const toggleSidebar = () => {}
+
   return (
     <div className="App">
-      <h1>Фьючерсы</h1>
-      <DealNamesList
-        clickedDealNames={clickedDealNames}
-        onNameClick={handleClick}
-      />
-      {openedDeals.length !== 0 ? (
-        <div className={styles.deals}>
+      <Sidebar />
+      <div className="mainContent">
+        <h1>Фьючерсы</h1>
+        <DealNamesList
+          clickedDealNames={clickedDealNames}
+          onNameClick={handleClick}
+        />
+        {openedDeals.length !== 0 ? (
+          <div className={styles.deals}>
+            <div className={styles.closedDeals}>
+              <ClosedDealList clickedDealNames={clickedDealNames} />
+            </div>
+            <div>
+              <h3>Открытые сделки</h3>
+              <OpenedDealList openedDeals={openedDeals} />
+            </div>
+          </div>
+        ) : (
           <div className={styles.closedDeals}>
             <ClosedDealList clickedDealNames={clickedDealNames} />
           </div>
-          <div>
-            <h3>Открытые сделки</h3>
-            <OpenedDealList openedDeals={openedDeals} />
-          </div>
-        </div>
-      ) : (
-        <div className={styles.closedDeals}>
-          <ClosedDealList clickedDealNames={clickedDealNames} />
-        </div>
-      )}
+        )}
+      </div>
     </div>
   )
 }
