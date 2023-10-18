6bf35c93074d13b82670348b5f140479e0bf6429
Natasha <nmalyushenko@mail.ru>
2023-10-18 19:22:47 +0300

    navigation with react router was added

--- src/App.js
+++ src/App.js
@@ -1,61 +1,18 @@
 import './App.css'
-import styles from './components/Deals/App.module.css'
-import DealNamesList from './components/Deals/DealNamesList'
-import ClosedDealList from './components/Deals/ClosedDealList'
-import OpenedDealList from './components/Deals/OpenedDealList'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { baseURL } from './components/Deals/ClosedDealList'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Archive from './components/Archive'
 
 export default function App() {
-  const [clickedDealNames, setClickedDealNames] = useState([])
-  const [buttonStyle, setButtonStyle] = useState('light')
-  const [openedDeals, setOpenedDeals] = useState([])
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await axios.get(`${baseURL}deals/open`)
-      const deals = await response.data
-      setOpenedDeals(deals)
-    }
-    fetchData()
-  }, [])
-
-  const handleClick = (clickedDealName) => {
-    const newClickedDealNames = [...clickedDealNames]
-
-    if (newClickedDealNames.includes(clickedDealName)) {
-      const indexToDelete = newClickedDealNames.indexOf(clickedDealName)
-      indexToDelete > -1 && newClickedDealNames.splice(indexToDelete, 1)
-    } else {
-      newClickedDealNames.push(clickedDealName)
-    }
-    setClickedDealNames(newClickedDealNames)
-    setButtonStyle(buttonStyle === 'light' ? 'success' : 'light')
-  }
-
   return (
-    <div className="App">
-      <h1>Фьючерсы</h1>
-      <DealNamesList
-        clickedDealNames={clickedDealNames}
-        onNameClick={handleClick}
-      />
-      {openedDeals.length !== 0 ? (
-        <div className={styles.deals}>
-          <div className={styles.closedDeals}>
-            <ClosedDealList clickedDealNames={clickedDealNames} />
-          </div>
-          <div>
-            <h3>Открытые сделки</h3>
-            <OpenedDealList openedDeals={openedDeals} />
-          </div>
-        </div>
-      ) : (
-        <div className={styles.closedDeals}>
-          <ClosedDealList clickedDealNames={clickedDealNames} />
-        </div>
-      )}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/archive" element={<Archive />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,60 @@
+import styles from './Home.module.css'
+import DealNamesList from './Deals/DealNamesList'
+import ClosedDealList from './Deals/ClosedDealList'
+import OpenedDealList from './Deals/OpenedDealList'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { baseURL } from './Deals/ClosedDealList'
+
+export default function Home() {
+  const [clickedDealNames, setClickedDealNames] = useState([])
+  const [buttonStyle, setButtonStyle] = useState('light')
+  const [openedDeals, setOpenedDeals] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`${baseURL}deals/open`)
+      const deals = await response.data
+      setOpenedDeals(deals)
+    }
+    fetchData()
+  }, [])
+
+  const handleClick = (clickedDealName) => {
+    const newClickedDealNames = [...clickedDealNames]
+
+    if (newClickedDealNames.includes(clickedDealName)) {
+      const indexToDelete = newClickedDealNames.indexOf(clickedDealName)
+      indexToDelete > -1 && newClickedDealNames.splice(indexToDelete, 1)
+    } else {
+      newClickedDealNames.push(clickedDealName)
+    }
+    setClickedDealNames(newClickedDealNames)
+    setButtonStyle(buttonStyle === 'light' ? 'success' : 'light')
+  }
+
+  return (
+    <>
+      <h1>Фьючерсы</h1>
+      <DealNamesList
+        clickedDealNames={clickedDealNames}
+        onNameClick={handleClick}
+      />
+      {openedDeals.length !== 0 ? (
+        <div className={styles.deals}>
+          <div className={styles.closedDeals}>
+            <ClosedDealList clickedDealNames={clickedDealNames} />
+          </div>
+          <div>
+            <h3>Открытые сделки</h3>
+            <OpenedDealList openedDeals={openedDeals} />
+          </div>
+        </div>
+      ) : (
+        <div className={styles.closedDeals}>
+          <ClosedDealList clickedDealNames={clickedDealNames} />
+        </div>
+      )}
+    </>
+  )
+}
