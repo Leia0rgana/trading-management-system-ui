d5e0796628642f36307a925b783ebf5992749db0
Natasha Malyushenko <nmalyushenko@mail.ru>
2024-01-23 22:50:33 +1000

    disabled strict mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { StrictMode } from 'react'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,9 +8,7 @@ import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
