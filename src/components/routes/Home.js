e969b1dce464becdda502bff77855c463534a716
Natasha Malyushenko <nmalyushenko@mail.ru>
2024-01-24 20:34:03 +1000

    resetFilters button added

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -2,12 +2,25 @@ import styles from './Home.module.css'
 import DealNamesList from '../Deals/DealNamesList'
 import ClosedDealList from '../Deals/ClosedDealList'
 import OpenedDealList from '../Deals/OpenedDealList'
+import Button from 'react-bootstrap/Button'
 import { useState, useEffect } from 'react'
+import { MdClear } from 'react-icons/md'
+import { IconContext } from 'react-icons'
 import axios from 'axios'
 import { BASE_URL } from '../Deals/ClosedDealList'
+import {
+  selectDealNamesFilter,
+  resetFilter,
+} from '../../redux/slices/filterSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
   const [openedDeals, setOpenedDeals] = useState([])
+  const dealNamesFilter = useSelector(selectDealNamesFilter)
+  const navigate = useNavigate()
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
     async function fetchData() {
@@ -21,7 +34,28 @@ export default function Home() {
   return (
     <div className="main-content">
       <h1>Фьючерсы</h1>
-      <DealNamesList />
+      <div className={styles.filter}>
+        {dealNamesFilter.length > 0 && (
+          <div className={styles.icon}>
+            <IconContext.Provider value={{ color: '#A8A8A8', size: '2rem' }}>
+              <Button
+                title="Сбросить фильтры"
+                variant="light"
+                className={styles.button}
+                onClick={() => {
+                  dispatch(resetFilter())
+                  navigate('/')
+                }}
+              >
+                <MdClear />
+              </Button>
+            </IconContext.Provider>
+          </div>
+        )}
+        <div className={styles.dealNames}>
+          <DealNamesList />
+        </div>
+      </div>
       {openedDeals.length !== 0 ? (
         <div className={styles.deals}>
           <div className={styles.closedDeals}>
