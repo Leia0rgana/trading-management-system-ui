064cc499b66ce2cca7f35968ad0ade610bab5c8c
Natasha Malyushenko <nmalyushenko@mail.ru>
2024-02-12 22:51:57 +0300

    deleted custom hook

--- src/hooks/useDealsQuery.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { fetchDeals } from '../services/deals'
-
-export default function useTodosQuery() {
-  return useQuery({
-    queryFn: () => fetchDeals(),
-    queryKey: ['deals'],
-  })
-}
