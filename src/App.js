350ffcdcb7579f3b8354ea08487c6a313294623e
Natasha Malyushenko <nmalyushenko@mail.ru>
2024-02-12 22:48:52 +0300

    app wrapped in QueryProvider

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Home from './components/routes/Home'
 import Archive from './components/routes/Archive'
 import MainLayout from './layouts/MainLayout'
 import PageNotFound from './components/routes/PageNotFound'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="archive" element={<Archive />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home />} />
+              <Route path="archive" element={<Archive />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
