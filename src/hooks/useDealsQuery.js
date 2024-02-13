d22d97bcb238eba14e23489d4422e986a4e67e8f
Natasha Malyushenko <nmalyushenko@mail.ru>
2024-02-13 16:12:25 +0300

    added custom hook useDealsQuery

---
+++ src/hooks/useDealsQuery.js
@@ -0,0 +1,9 @@
+import { useQuery } from '@tanstack/react-query'
+import { fetchDeals } from '../services/deals'
+
+export default function useDealsQuery(key, endpoint) {
+  return useQuery({
+    queryKey: [key],
+    queryFn: () => fetchDeals(endpoint),
+  })
+}
